7297eb26f8e026a101e3a6bf4f3449de8be9dcef
elvinhaxverdiyev <elvinhaxverdiyev777@gmail.com>
2024-11-10 14:24:57 +0400

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,17 +1,43 @@
-# filter.py
-
 def filter_by_cake_type(stock, cake_type):
-    """tortlari nove gore filerlemek"""
+    """Tortlari novune gore filterlemek"""
     filtered = {}
-    for cake, price in stock.items():
-        if cake_type.lower() in cake.lower():
-            filtered[cake] = price
+    try:
+        for cake, price in stock.items():
+            if cake_type.lower() in cake.lower():
+                filtered[cake] = price
+    except AttributeError as e:
+        print(f"Xeta: 'cake' ve ya 'cake_type' deyiseni duzgun formatda deyil. {e}")
+    except Exception as e:
+        print(f"Qeyri-mueyyen xeta bas verdi filter_by_cake_type funksiyasinda: {e}")
     return filtered
 
 def filter_by_price_range(stock, min_price, max_price):
-    """qiymete gore filterlemek"""
+    """Qiymete gore filterlemek"""
     filtered = {}
-    for cake, price in stock.items():
-        if min_price <= price <= max_price:
-            filtered[cake] = price
+    try:
+        for cake, price in stock.items():
+            if min_price <= price <= max_price:
+                filtered[cake] = price
+    except TypeError as e:
+        print(f"Xeta: Qiymet deyerleri arasinda uygunluq yoxlanilarken sehv bas verdi: {e}")
+    except Exception as e:
+        print(f"Qeyri-mueyyən xeta bas verdi filter_by_price_range funksiyasinda: {e}")
     return filtered
+
+def filtered_by_start_with_letter(stock, letter):
+    """Basladigi herfe gore filterlemek"""
+    filtered = {}
+    try:
+        if not isinstance(letter, str) or len(letter) != 1:
+            raise ValueError("Verilen 'letter' parametri duzgun bir herf olmalidir.")
+        for cake, price in stock.items():
+            if cake[0].lower() == letter.lower():
+                filtered[cake] = price
+    except ValueError as e:
+        print(f"Xeta: {e}")
+    except KeyError as e:
+        print(f"Xeta: Stockda {e} adli tort tapilmadi.")
+    except Exception as e:
+        print(f"Qeyri-mueyyen xeta bas verdi filtered_by_start_with_letter funksiyasinda: {e}")
+    return filtered
+
